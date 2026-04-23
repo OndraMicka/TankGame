60d71783f06ec73fac62fd3bd357d398b787a846
ondrejMicka <ondrej.micka08@gmail.com>
2026-04-23 18:07:13 +0200

    Revert "finished class game map, need to read images to load maps to game"
    This reverts commit 3e52d2ba925396b5825a5216fd458a895d85c2a3.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 import core.ConfigManager;
 import ui.MyFrame;
 
-import javax.swing.*;
-
 
 public class Main {
     public static void main(String[] args) {
-        ConfigManager.loadGameSettingsResources("settings.json");
-        SwingUtilities.invokeLater(MyFrame::new);
+        ConfigManager.loadGameSettingsResources( "settings.json");
+        new MyFrame();
 
 
     }
--- src/maps/AllMaps.java
+++
@@ -1,9 +0,0 @@
-package maps;
-
-import java.util.ArrayList;
-
-public class AllMaps {
-    private ArrayList<GameMap> maps;
-    public AllMaps() {
-    }
-}
